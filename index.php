50a675c7297c5dc29329df96c9e9bccf563f4173
Dclasyc <Akarigbooladipo@gmail.com>
2022-06-17 11:20:26 +0100

    Intial commit

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="finalproject.css">
+
+	<title>Tailors Hub</title>
+</head>
+<body>
+
+
+			<!-- Navigation panel -->
+
+
+		<div id="navdiv">
+
+		<nav class="navbar navbar-expand-lg">
+		  <div class="container-fluid">
+		    <a class="navbar-brand" href="index.php">
+		      <img src="images/navlogo.jpg" alt="logo" height="50">
+		    </a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		      <ul class="navbar-nav me-auto mb-md-0">
+		        <li class="nav-item">
+		        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+		        </li>
+		        <li class="nav-item" class="navreg-item">
+		          <a class="nav-link" href="signin.php">Log In</a>
+		        </li>
+		        <li class="nav-item" class="navreg-item">
+		          <a class="nav-link" href="signup.php">Sign UP</a>
+		        </li>
+		        
+		      </ul>
+		      <form class="d-flex">
+		        <input class="form-control me-2" type="search" placeholder="Type search here" aria-label="Search">
+		        <button class="btn" id="btnsearch" type="submit">Search</button>
+		      </form>
+		    </div>
+		  </div>
+		</nav>
+						
+			</div>
+
+
+
+		<!-- BODY OF WEBPAGE -->
+
+<div class="container-fluid" id="parentdiv" style="background-color: #f4f5f6;">	
+
+
+	<div class="row mb-3">
+
+
+		<div class="col-md-12" id="adsdiv">
+			
+			<!-- Ads Div -->
+
+
+		</div>
+		
+	</div>
+
+
+	<div class="row mb-3">
+		
+		<div class="col-md-3" id="sidepanel">
+
+			<div class="row sidepanellist" class="sidepanellist" id="menscasual">
+				
+			</div>
+
+			<div class="row sidepanellist" id="womenscasual">
+				
+			</div>
+
+			<div class="row sidepanellist" id="mensnative">
+				
+			</div>
+
+			<div class="row sidepanellist" id="womensnative">
+				
+			</div>
+
+			<div class="row sidepanellist" id="mensformal">
+				
+			</div>
+
+			<div class="row sidepanellist" id="womensformal">
+				
+			</div>
+
+			<div class="row sidepanellist" id="menswedding">
+				
+			</div>
+
+			<div class="row sidepanellist" id="womenswedding">
+				
+			</div>
+
+
+
+
+
+		</div>
+
+
+		<div class="col-md-8" id=indexpicsdiv>
+			
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+				<div class="landingpagepics">
+					
+				</div>
+
+
+
+		</div>
+
+
+	</div>
+		
+	</div>
+
+
+
+
+	<!-- FOOTER SECTION -->
+
+	<div id=footerdiv>
+
+		<div id="overlay"></div>
+		
+	</div>
+
+
+</body>
+</html>
